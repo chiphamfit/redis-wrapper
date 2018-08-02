033148e9f9532c7f3506f3e293c0c553463bd4f7
thanhchifit <thanhchi.fit.hcmus@gmail.com>
2018-08-02 16:56:53 +0700

    small change

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 import mongodb from 'mongodb';
 import redis from 'redis';
-import * as app from './app'; 
+import * as index from './index'; 
 
 const test = async () => {
   const redisClient = redis.createClient();
   const mongoClient = mongodb.connect('mongodb://localhost:27017/demo', {
     useNewUrlParser: true
   });
-  const client = await app.createClient(mongoClient, redisClient);
+  const client = await index.createClient(mongoClient, redisClient);
   const docs = await client.collection('inventory').find({});
-  console.log(docs);
+  // console.log(docs);
   // console.log(collections);
 }
 
