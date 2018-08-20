d5c719fcfbe6f8df4c8cd5f8e26c15d3145336c6
Chi Pham <chipham@kms-technology.com>
2018-08-20 11:48:44 +0700

    change directory

---
+++ src/wrapper.js
@@ -0,0 +1,79 @@
+// Dependences
+const MongoClient = require('mongodb').MongoClient;
+const RedisWrapper = require('./redis_wrapper').RedisWrapper;
+const LazyCollection = require('./wrapper/lazy_collection');
+const ThroughCollection = require('./wrapper/lazy_collection').LazyCollection;
+
+// Constants
+const NO_EXPIRE = -1;
+const URL = 'mongodb://localhost:27017/test';
+const OPTION = {
+  useNewUrlParser: true
+};
+
+class Wrapper {
+  constructor(mongo, redis, expire) {
+    if (!(mongo instanceof MongoClient)) {
+      MongoClient.connect(URL, OPTION, (error, client) => {
+        if (error) {
+          throw error;
+        }
+
+        mongo = client;
+      });
+    }
+
+    this.mongo = mongo;
+    this.redisWrapper = new RedisWrapper(redis, expire);
+    this.lazy = this.redisWrapper.expire === NO_EXPIRE ? false : true;
+  }
+
+  async connect() {
+    if (this.mongo instanceof Promise) {
+      this.mongo = await this.mongo;
+    }
+
+    return this;
+  }
+
+  db(dbName, options) {
+    if (!this.mongo.isConnected()) {
+      throw new Error('Mongo Client must connect before create db');
+    }
+
+    // create db, override db.collection() method
+    let db = this.mongo.db(dbName, options);
+    db.lazy = this.lazy;
+    db.redisWrapper = this.redisWrapper;
+    db.cacheCollection = function(name, options) {
+      const collection = this.collection(name, options);
+      if (this.lazy) {
+        return new LazyCollection(collection, this.redisWrapper);
+      }
+
+      return new ThroughCollection(collection, this.redisWrapper);
+    };
+
+    return db;
+  }
+
+  isConnected() {
+    if (!this.mongo.isConnected) {
+      return false;
+    }
+
+    this.redis.on('connect', (error) => {
+      if (error) {
+        return false;
+      }
+    });
+
+    return this.mongo.isConnected();
+  }
+
+  async clearCache() {
+    await this.redisWrapper.clearCache();
+  }
+}
+
+module.exports = Wrapper;
